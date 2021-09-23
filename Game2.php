4676c8b206bb097a293736647fbc783dbe0abbb5
Deep Shah <shah.deep095@gmail.com>
2021-09-23 10:58:16 +0530

    third commit

---
+++ Game2.php
@@ -0,0 +1,36 @@
+<!-- Arrays Concept in PHP
+
+=> Arrays starts from 0th index
+=> Arrays actually means a collection of single kind of values
+=> i.e, name_of_array[index]; -syntex-
+=>we can also modify values via the action of assignment like this,
+
+$ARRAY_NAME = "2,3,4";
+
+$ARRAY_NAME[2] = "5";
+
+OUTPUT IS:
+$ARRAY_NAME = [2,3,5]
+
+=> We can also access individual element from an array.
+=>count() function is used to count how many elements contained in an array.
+=>
+
+-->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Game Part 2</title>
+  </head>
+  <body>
+
+  </body>
+</html>
+<?php
+
+
+
+
+?>
